dc033037711766b1cdf3d7fe5e0e3cd845b0f50b
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-11-28 15:28:36 +0700

    Melengkapi gradle.app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.compose.icons)
+
     //Room
     implementation(libs.bundles.room)
     ksp(libs.room.compiler)
