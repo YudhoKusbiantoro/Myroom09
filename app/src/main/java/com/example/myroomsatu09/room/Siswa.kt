6d77b897fa6fe33ea25b055112ca84fbdd529859
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-11-28 15:36:57 +0700

    Melengkapi Room file Siswa.kt

--- app/src/main/java/com/example/myroomsatu09/room/Siswa.kt
+++ app/src/main/java/com/example/myroomsatu09/room/Siswa.kt
@@ -1,2 +1,13 @@
 package com.example.myroomsatu09.room
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa (
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
