4c4b1ba5d4001999edf5e718413cccfa860bd11e
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-11-28 15:46:07 +0700

    Melengkapi repositori file ContainerApp.kt.kt

--- app/src/main/java/com/example/myroomsatu09/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroomsatu09/repositori/ContainerApp.kt
@@ -1,1 +1,30 @@
 package com.example.myroomsatu09.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.myroomsatu09.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa: RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp{
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OffLineRepositoriSiswa(
+                    DatabaseSiswa.getDatabase(context).siswaDao())
+            }
+        }
+
+class AplikasiSiswa: Application() {
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya untuk
+     */
+
+    lateinit var container : ContainerApp
+
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
